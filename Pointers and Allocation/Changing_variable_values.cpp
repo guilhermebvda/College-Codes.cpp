f93cd1220af36387ba1bb73af2cd1d5cde65af36
Guiherme Barros <guilhermebvda@alu.ufc.br>
2022-02-16 23:07:46 -0300

    Task with Pointers or Allocation

--- Pointers and Allocation/Changing_variable_values.cpp
+++ Pointers and Allocation/Changing_variable_values.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-// Implemente a função 'troca'.
 void troca(int *a, int *b)
 {
     int trocar;
@@ -15,7 +14,6 @@ int main()
    std::cin >> x;
    std::cin >> y;
    
-   // Chame a função 'troca' para trocar os valores de x e y.
    troca(&x,&y);
    
    std::cout << x << " " << y << std::endl;
