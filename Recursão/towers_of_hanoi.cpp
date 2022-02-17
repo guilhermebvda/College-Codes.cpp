fe7d525f5349e25fd8d5d7f80e4c090e00be7c45
Guiherme Barros <guilhermebvda@alu.ufc.br>
2022-02-16 23:35:07 -0300

    Tasks of Recursion

---
+++ Recursão/towers_of_hanoi.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+
+void Torre_de_Hanoi(char origem, char destino, char auxiliar, int discos)
+{
+   if(discos == 1){
+      cout << origem << " -> " << destino << endl;
+   }else{
+      Torre_de_Hanoi(origem,auxiliar,destino,discos-1);
+      cout << origem << " -> " << destino << endl;
+      Torre_de_Hanoi(auxiliar, destino, origem,discos-1);
+   }
+}
+
+
+int main(){
+   int discos;
+   cin >> discos;
+   
+   Torre_de_Hanoi('A','C','B',discos);
+
+   return 0;
+}
